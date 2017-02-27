c217035b565e5c9615902c859d6e4533974a2966
Alex Lombardo <alexlombardo21@gmail.com>
2017-02-27 18:25:21 +0000

    Update problem4.js

--- ProjectEuler/problem4.js
+++ ProjectEuler/problem4.js
@@ -1,6 +1,25 @@
 function isPalindrome(someNumber){
-    var numberAsString = someNumber.toString();
-    if (numberAsString.length % 2 !== 0) {
-        numberAsString.splice(Math.floor(numberAsString.length / 2), 1);
+    var numberAsArray = someNumber.toString(10).split("");
+    if (numberAsArray.length % 2 !== 0) {
+        numberAsArray.splice(Math.floor(numberAsArray.length / 2), 1);
+    }
+    var numberIsPalindrome = true;
+    for (var i = 0; ((i < numberAsArray.length) && numberIsPalindrome); i++){
+        if (numberAsArray[i] !== numberAsArray[numberAsArray.length - 1 - i]){
+            numberIsPalindrome = false;
         }
-        
+    }
+    return numberIsPalindrome;
+}
+
+var highestFound = [0, 0, 0];
+for (var x = 999; x > 0; x--){
+    for (var y = 999; y > 0; y--){
+        if (x * y > highestFound[0]){
+            if (isPalindrome(x * y)){
+                highestFound = [x * y, x, y];
+            }
+        }
+    }
+}
+console.log(highestFound);
