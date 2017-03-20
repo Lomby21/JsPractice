dfb8abcffdac683e7fb39c7018e659f0c58fcfaf
Alex Lombardo <alexlombardo21@gmail.com>
2017-03-20 19:36:34 +0000

    Update recursive-permutations.js

--- InterviewCake/recursive-permutations.js
+++ InterviewCake/recursive-permutations.js
@@ -2,12 +2,13 @@ function permutations(someString) {
   if (someString.length > 1) {
     var shorterString = someString.slice(1);
     permutations(shorterString);
-    var newPermutationSet = {};
+    var newPermutationSet = [];
     for (var i = 0; i < permutationSet.length; i++) {
       var splitPermutation = permutationSet[i].split("");
       for (var j = 0; j <= splitPermutation.length; j++) {
-        var newPermutation = splitPermutation.splice(j, 0, someString[0]);
-        newPermutationSet.push(toString(newPermutation));
+        var newPermutation = splitPermutation;
+        newPermutation.splice(j, 0, someString[0]);
+        newPermutationSet.push(newPermutation.join(""));
       }
     } 
     permutationSet = newPermutationSet;
@@ -16,7 +17,7 @@ function permutations(someString) {
   }
 }
 
-var permutationSet = {};
+var permutationSet = [];
 var someString = '';
 
 permutations(someString);
